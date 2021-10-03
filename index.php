7c705529e63a7b698fa19365a29413326afa4691
alvaroeduardo <alvaroedusilva@yahoo.com>
2021-10-03 15:35:39 -0300

    Adicionar imagem do candidato

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
                         <button>Next</button>
                     </footer>
                 </section> -->
+                
 
                 <?php
                     // CATEGORIAS
@@ -43,16 +44,42 @@
                     $indicadoQuery = "SELECT * FROM indicados INNER JOIN indicacoes ON (indic = id_indic)";
                     $indicadoResult = mysqli_query($conn, $indicadoQuery);
 
+                    // ESCOLHE A IMAGEM DA PESSOA CONFORME A NACIONALIDADE DELA
+                    function imgUser($nasc, $name){
+                        // https://www.habbo.com.br/habbo-imaging/avatarimage?img_format=png&user=ferrazmatheus&direction=2&head_direction=3&size=l&gesture=sml&action=std,wav&frame=3
+
+                        $hhbr = ".com.br"; // BRASIL
+                        $hhde = ".de"; // ALEMANHA
+                        $hhes = ".es"; // ESPANHA
+                        $hhfi = ".fi"; // FINLÂNDIA
+                        $hhfr = ".fr"; // FRANÇA
+                        $hhnl = ".nl"; // HOLANDA
+                        $hhus = ".com"; // ESTADOS UNIDOS
+                        $hhit = ".it"; // ITÁLIA
+                        $hhtr = ".tr"; // TURQUIA
+
+                        if($nasc == "hhbr"){
+                            return "https://www.habbo". $hhbr ."/habbo-imaging/avatarimage?img_format=png&user=". $name ."&direction=2&head_direction=3&size=l&gesture=sml&action=std,wav&frame=3";
+                        }
+
+                    }
+
                     while($row_cat = mysqli_fetch_assoc($catResult)){
                         echo "<section id='ctgr_". $row_cat['id_ctgr'] ."' class='ctgr'>";
 
                         while($row_indic = mysqli_fetch_assoc($indicResult)){
-                            echo "<div id='indc_". $row_indic['ctgr'] ."' class='indic'>";
-                            
-                                while($row_indicado = mysqli_fetch_assoc($indicadoResult)){
-                                    echo "Nome:",$row_indicado['name'];
-                                }
+                            echo "<div id='indic_". $row_indic['ctgr'] ."' class='indic'>";
+                                echo "<h2>". $row_indic['indic_name'] ."</h2>";
+                                echo "<hr></hr>";
 
+                                while($row_indicado = mysqli_fetch_assoc($indicadoResult)){
+                                    echo "<div class='indicado'>";
+                                    echo "<img src='". imgUser($row_indicado['nasc'], $row_indicado['name']) ."'>";
+
+                                    echo "<input type='radio' value='". $row_indicado['name'] ."' name='". $row_indicado['name'] ."'>". $row_indicado['name'] ."</input>";
+
+                                    echo "</div>";
+                                }
                             echo "</div>";
                         }
 
